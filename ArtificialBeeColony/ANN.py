6ba94995edad064da35787df731885cc31c09d71
kinan11 <kingamiszczak1@op.pl>
2022-06-15 15:26:37 +0200

    final commit

--- ArtificialBeeColony/ANN.py
+++ ArtificialBeeColony/ANN.py
@@ -5,24 +5,22 @@ def relu(inpt):
     result[inpt<0] = 0
     return result
 
-def predict_outputs(weights_mat, data_inputs, data_outputs, activation="relu"):
+def predict_outputs(weights_mat, data_inputs, data_outputs):
     predictions = np.zeros(shape=(data_inputs.shape[0]))
     for sample_idx in range(data_inputs.shape[0]):
         r1 = data_inputs[sample_idx, :]
         for curr_weights in weights_mat:
             r1 = np.matmul(r1, curr_weights)
-            if activation == "relu":
-                r1 = relu(r1)
         predicted_label = np.where(r1 == np.max(r1))[0][0]
         predictions[sample_idx] = predicted_label
     correct_predictions = np.where(predictions == data_outputs)[0].size
     accuracy = (correct_predictions/data_outputs.size)*100
     return accuracy, predictions
     
-def fitness(weights_mat, data_inputs, data_outputs, activation="relu"):
+def fitness(weights_mat, data_inputs, data_outputs):
     accuracy = np.empty(shape=(weights_mat.shape[0]))
     for sol_idx in range(weights_mat.shape[0]):
         curr_sol_mat = weights_mat[sol_idx, :]
-        accuracy[sol_idx], _ = predict_outputs(curr_sol_mat, data_inputs, data_outputs, activation=activation)
+        accuracy[sol_idx], _ = predict_outputs(curr_sol_mat, data_inputs, data_outputs)
     return accuracy
 
